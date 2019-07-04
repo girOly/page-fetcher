576ef06b7085140e16f811786d47512c04b0f69f
girOly <girardob@gmail.com>
2019-07-04 21:08:22 +0000

    fetch page using terminal

---
+++ fetcher.js
@@ -0,0 +1,14 @@
+const args = process.argv.slice(2);
+const fs = require('fs');
+const request = require('request');
+request = function(args[1], (error, response, body)
+fs.writeFile(args[0], body, error {
+  if (error) {
+    return (error);
+  }
+  let bigStuff = statSync.size;
+  let statSync = fs.statSync(args[1]);
+  console.log(`Downloaded and saved ${bigStuff} bytes to ./index.html`);
+
+});
+});
