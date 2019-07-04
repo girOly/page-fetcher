6526595699efaa5518639e68b608c29c625a52cc
girOly <girardob@gmail.com>
2019-07-04 21:41:33 +0000

    Bug Fix

--- fetcher.js
+++ fetcher.js
@@ -1,14 +1,15 @@
 const args = process.argv.slice(2);
-const fs = require('fs');
-const request = require('request');
-request = function(args[1], (error, response, body)
-fs.writeFile(args[0], body, error {
-  if (error) {
-    return (error);
-  }
-  let bigStuff = statSync.size;
-  let statSync = fs.statSync(args[1]);
-  console.log(`Downloaded and saved ${bigStuff} bytes to ./index.html`);
+const fs = require("fs");
+const request = require("request");
 
-});
+
+request(args[0], (error, response, body) => {
+  fs.writeFile(args[1], body, function(err) {
+    if (error) {
+      return console.log(error);
+    }
+    const statsSync = fs.statSync(args[1]);
+    const bigStuff = stats.size;
+    console.log(`Downloaded and saved ${bigStuff} bytes to ./index.html`);
+  });
 });
